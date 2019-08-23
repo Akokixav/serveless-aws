de290969fa6b4ef01ba73fdc3d26b511a924e719
EC2 Default User <ec2-user@ip-172-31-28-26.us-west-2.compute.internal>
2019-08-23 03:59:22 +0000

    Cognito settings

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_z99yan9cZ', 
+        userPoolClientId: '7gtot9vhf756dvlga8vvm6drji', 
+        region: 'us-west-2'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
